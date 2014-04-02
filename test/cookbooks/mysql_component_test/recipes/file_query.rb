b078d9f771f755510359aef693ecc39477a05950
Anton Butovich <abutovich@gmail.com>
2014-04-02 13:27:42 +0300

    fixed kitchen user_create privileges, recipe file_query_test

--- test/cookbooks/mysql_component_test/recipes/file_query.rb
+++ test/cookbooks/mysql_component_test/recipes/file_query.rb
@@ -1,4 +1,4 @@
 include_recipe "mysql_component"
-include_recipe "mysql_component::db_create"
+include_recipe "mysql_component::db_manage"
 include_recipe "mysql_component::file_query"
 include_recipe "minitest-handler"
