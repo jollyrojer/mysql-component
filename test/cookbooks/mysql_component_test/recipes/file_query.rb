22eaad650de7edc5dbd6855aea88ef34910e9ea9
Anton Butovich <abutovich@gmail.com>
2014-04-02 15:15:56 +0300

    fixed file_query test

--- test/cookbooks/mysql_component_test/recipes/file_query.rb
+++ test/cookbooks/mysql_component_test/recipes/file_query.rb
@@ -1,4 +1,5 @@
 include_recipe "mysql_component"
 include_recipe "mysql_component::db_manage"
+include_recipe "mysql_component::user_manage"
 include_recipe "mysql_component::file_query"
 include_recipe "minitest-handler"
