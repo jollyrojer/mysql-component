9432fbe69dadc1d77dd82037a707332136402dca
Anton Butovich <abutovich@gmail.com>
2014-04-04 17:57:05 +0300

    added timeouts before actions in tests

--- test/cookbooks/mysql_component_test/recipes/file_query.rb
+++ test/cookbooks/mysql_component_test/recipes/file_query.rb
@@ -1,5 +1,10 @@
 include_recipe "mysql_component"
+
+execute "sleep 30"  # Wait for MySQL startup
+
 include_recipe "mysql_component::db_manage"
 include_recipe "mysql_component::user_manage"
 include_recipe "mysql_component::file_query"
 include_recipe "minitest-handler"
+
+execute "sleep 3"  # Wait for MySQL startup
